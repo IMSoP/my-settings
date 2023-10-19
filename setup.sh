dd776341619c280cdf7c6be07d5bd2f2701cc7e8
Rowan Tommins <tomminsr@holidaytaxis.com>
2023-10-19 13:34:04 +0100

    Self-referencing setup script for quick propogation

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git clone git@github.com:IMSoP/my-settings ~/my-settings
+for file in .ackrc .bashrc .gitconfig .screenrc .vimrc
+do
+    if [ -L ~/$file ]
+    then
+        echo -n "~/$file is already a symlink: "
+        ls -l ~/$file
+    elif [ -f ~/$file ]
+    then
+        mv --backup=numbered $file $file.backup
+	ln -s ~/my-settings/linux-home/$file ~/$file
+    else
+	ln -s ~/my-settings/linux-home/$file ~/$file
+    fi
+done
+echo
+echo 'All done; run the following to load bash settings:'
+echo '. ~/.bashrc'
